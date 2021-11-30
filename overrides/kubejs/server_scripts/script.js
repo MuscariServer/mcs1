6ed71de6eeb7e1b49d374d2bb008c36ccaa2e33a
tier940 <tsubasa.ito@relightings.com>
2021-11-30 14:42:18 +0900

    [update] GameStageで無理やり対応

--- overrides/kubejs/server_scripts/script.js
+++ overrides/kubejs/server_scripts/script.js
@@ -8,12 +8,20 @@ settings.logErroringRecipes = true
 console.info('Hello, World! (You will see this line every time server resources reload)')
 
 // Listen to player login event
-onEvent('player.logged_in', event => {
+onEvent('player.tick', event => {
   if (!event.player.stages.has('starting_items')) {
     // Add the stage
     event.player.stages.add('starting_items')
 
     // Give some items to player
     event.player.give(Item.of('waystones:warp_stone', '{Damage:0}'))
+  }
+  
+  if (event.player.stages.has('vote_reward')) {
+    // Give some items to player
+    event.player.give(Item.of('inventorypets:pet_meta', '{Damage:0}'))
+	
+	  // Remove the stage
+    event.player.stages.remove('vote_reward')
   }
 })
